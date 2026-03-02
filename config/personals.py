0535e2e64ca69179890d4e7fcb79f6b20cac5639
dheerajsingh718 <dheerajsingh718@gmail.com>
2026-03-02 13:20:31 -0500

    Personal Data Changed

--- config/personals.py
+++ config/personals.py
@@ -19,12 +19,12 @@ version:    26.01.20.5.08
 # >>>>>>>>>>> Easy Apply Questions & Inputs <<<<<<<<<<<
 
 # Your legal name
-first_name = "Sai"                 # Your first name in quotes Eg: "First", "Sai"
-middle_name = "Vignesh"            # Your name in quotes Eg: "Middle", "Vignesh", ""
-last_name = "Golla"                # Your last name in quotes Eg: "Last", "Golla"
+first_name = "Dheeraj"                 # Your first name in quotes Eg: "First", "Sai"
+middle_name = "Singh"            # Your name in quotes Eg: "Middle", "Vignesh", ""
+last_name = "Shankar"                # Your last name in quotes Eg: "Last", "Golla"
 
 # Phone number (required), make sure it's valid.
-phone_number = "9876543210"        # Enter your 10 digit number in quotes Eg: "9876543210"
+phone_number = "2017806773"        # Enter your 10 digit number in quotes Eg: "9876543210"
 
 # What is your current city?
 current_city = "Jersey City"                  # Los Angeles, San Francisco, etc.
@@ -33,20 +33,20 @@ Note: If left empty as "", the bot will fill in location of jobs location.
 '''
 
 # Address, not so common question but some job applications make it required!
-street = ""
+street = "394 Baldwin Ave"
 state = "New Jersey"
-zipcode = ""
+zipcode = "07306"
 country = "United States"
 
 ## US Equal Opportunity questions
 # What is your ethnicity or race? If left empty as "", tool will not answer the question. However, note that some companies make it compulsory to be answered
-ethnicity = "Decline"              # "Decline", "Hispanic/Latino", "American Indian or Alaska Native", "Asian", "Black or African American", "Native Hawaiian or Other Pacific Islander", "White", "Other"
+ethnicity = "Asian"              # "Decline", "Hispanic/Latino", "American Indian or Alaska Native", "Asian", "Black or African American", "Native Hawaiian or Other Pacific Islander", "White", "Other"
 
 # How do you identify yourself? If left empty as "", tool will not answer the question. However, note that some companies make compulsory to be answered
-gender = "Decline"                 # "Male", "Female", "Other", "Decline" or ""
+gender = "Male"                 # "Male", "Female", "Other", "Decline" or ""
 
 # Are you physically disabled or have a history/record of having a disability? If left empty as "", tool will not answer the question. However, note that some companies make it compulsory to be answered
-disability_status = "Decline"      # "Yes", "No", "Decline"
+disability_status = "No"      # "Yes", "No", "Decline"
 
 veteran_status = "Decline"         # "Yes", "No", "Decline"
 ##
